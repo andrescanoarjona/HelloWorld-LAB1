cd7e091ea5f83756d2cfd0dd9f0066c85986109c
andres <aca00115@red.ujaen.es>
2023-09-25 09:55:57 +0300

    improvements naming variables.

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -12,7 +12,9 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv;
-    int n = 0, aux, MIN =1 , MAX = 5,MEM = 4;
+    int n = 0, mem = 4;
+    int aux;
+    private static final int MIN =1 , MAX = 5;
     Random RANDOM = new Random();
 
 
@@ -20,18 +22,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView)findViewById(R.id.tv_main_label);
     }
     @SuppressLint("SetTextI18n")
-    public void onBtChangeTextClick(View v)
-    {
-        tv = (TextView)findViewById(R.id.tv_main_label);
+    public void onBtChangeTextClick(View v) {
         ++n;
 
-        if(n<2)
+        if(n<2) {
             tv.setText("1º LAB of Mobile Application Development Techniques.");
-        else
-            tv.setText("Is the "+n+"º time that you change the text");
-
+        }
+        else {
+            tv.setText("Is the " + n + "º time that you change the text");
+        }
     }
 
     public void onBtChangeColor(View v)
@@ -39,16 +42,24 @@ public class MainActivity extends AppCompatActivity {
 
         do {
             aux = RANDOM.nextInt(MAX - MIN) + MIN;
-        }while(MEM == aux);
-        MEM = aux;
-
-        tv = (TextView)findViewById(R.id.tv_main_label);
+        }while(mem == aux);
+        mem = aux;
 
         switch (aux){
-            case 1:tv.setTextColor(Color.GREEN); break;
-            case 2:tv.setTextColor(Color.BLUE);break;
-            case 3:tv.setTextColor(Color.MAGENTA);break;
-            default:tv.setTextColor(Color.BLACK);
+            case 1: {
+                tv.setTextColor(Color.GREEN);
+                break;
+            }
+            case 2:{
+                tv.setTextColor(Color.BLUE);
+                break;
+            }
+            case 3:{
+                tv.setTextColor(Color.MAGENTA);
+                break;}
+            default:{
+                tv.setTextColor(Color.BLACK);
+            }
 
         }
     }
